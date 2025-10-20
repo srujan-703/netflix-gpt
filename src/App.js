5da3e07657a617c717b7962be407cc2705ed177d
Srujan <srujanlolam63@gmail.com>
2025-10-20 20:07:46 +0530

    initial set up and tailwind css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-bold text-green-800 text-3xl">
+      Namasthe everyone - lets build netflix gpt from scratch
+    </div>
+  );
+}
+
+export default App;
